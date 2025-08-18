ed74b4d88b3822d217e3b91dae6431950bd8dd9f
MilyonKifleyesus <mkifleye@my.centennialcollege.ca>
2025-08-17 21:46:07 -0400

    Remove all duplicate imageLoaded declarations

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -54,7 +54,6 @@ const HeroSection: React.FC = () => {
         {/* Gradient overlay for depth */}
         <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-transparent to-black/40"></div>
       </div>
-      <div className="absolute inset-0">
         <img
           src="https://images.pexels.com/photos/3729464/pexels-photo-3729464.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080"
           alt="Luxury automotive showroom"
